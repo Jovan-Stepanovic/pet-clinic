eb4c14e968135f19736f90cd4f341297cc2dc298
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-08-17 16:02:47 +0200

    Refactored services to common interface. closes #10

--- pet-clinic-data/src/main/java/com/js/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/js/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package com.js.petclinic.services;
 
 import com.js.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/com/js/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/js/petclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package com.js.petclinic.services;
 
-import com.js.petclinic.model.Owner;
 import com.js.petclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Owner vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 }
