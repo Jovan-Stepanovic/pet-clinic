db50fc171cffb60f8104c405e8b6a49bd20b7ce8
Jovan-Stepanovic <Jovan.Stepanowic@nordstrom.com>
2023-08-18 15:01:37 +0200

    Updated Vets list view and controller. closes #14

--- pet-clinic-web/src/main/java/com/js/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/js/petclinic/controller/VetController.java
@@ -1,13 +1,22 @@
 package com.js.petclinic.controller;
 
+import com.js.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String getVetsList() {
+    public String getVetsList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/vetsList";
     }
 }
